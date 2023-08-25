c9a82d9ec5e7d69fd0e78b35729be56f5e9e0f32
unknown <nandustrings6@gmail.com>
2023-08-24 21:48:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Project Title 
+"Nandini's Password Generator"
+
+# Project Description
+This website is deployed to enable users to randomly generate passwords that meet a stipulated criteria. This tool empowers users to generate a strong passwords great reducing risks of user accounts from getting hacked or taken over. 
+
+# Coding applications used to develop the website 
+
+The website was developed using basic HTML, CSS and JavaScript coding. 
+
+# User story
+
+* User clicks on "generate" button which will, prompt user with the following option; 
+    - Choose the lenght of the password within a pre-set criteria of between 8 to 128 characters length 
+    - Choose upper case in password
+    - Choose lower case in password
+    - Choose special charaters in password
+    - Choose numerals in password 
+* If user entered password length does not meet the required criteria the user is prompted to enter the correct length criteria once again. 
+* At any point user can choose to abandon the request by clicking cancel. 
+* Once all the criteria have been choose a random password is displayed for user 
+
+# Website screenshots 
+
+The following image shows the web application's appearance and functionality:
+
+![Password Generator Application.](./Assets/Mockimages/page1.png)
+![Password Generator Application.](./Assets/Mockimages/Page2.png)
+![Password Generator Application.](./Assets/Mockimages/Page3.png)
+![Password Generator Application.](./Assets/Mockimages/page4.png)
+![Password Generator Application.](./Assets/Mockimages/Page5.png)
+![Password Generator Application.](./Assets/Mockimages/page6.png)
+
+* If user does not choose any criteria or input an alphabet instead of a number the following error prompts are provided
+![Password Generator Application.](./Assets/Mockimages/page7.png)
+![Password Generator Application.](./Assets/Mockimages/page8.png)
+
+
+
+# Submission 
+
+* The URL of the deployed application.
+<<TBD>
+
+* The URL of the GitHub repository, with a unique name and a readme describing the project.
+<<tbd>
+
+- - -
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+
+**Updated new repo
---
+++ script.js
@@ -0,0 +1,61 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//your created code ----generatePassword function is what needs to be created
+
+function generatePassword() {
+   
+  var keyLength = parseInt(prompt("Password must be between 8 to 128 character, choose your password length"));
+  var upperCase = confirm("Your password must contain an uppercase alphabet, click ok to choose");
+  var lowerCase = confirm("Your password must contain a lowercase alphabet, click ok to choose");
+  var specialCharacters = confirm("Your password must contain special characters, click ok to choose");
+  var numerals = confirm("Your password must contain numbers, click ok to choose");
+
+  var upperCaseCriteria ="ABCDEFGHIJKLMNOPQRSTUVWXZ";
+  var lowerCaseCriteria ="abcdefghijklmnopqrstuvwxyz";
+  var specialChacCriteria ="!@#$%^&*()?.<\>|=+:;,[-_]";
+  var numeralsCriteria ="0123456789";
+  var multiSelect =[];
+
+  if (keyLength < 8 || keyLength >= 128) {
+    alert("Your password character length does not match the required criteria, please try again")
+    var keyLength = prompt("Password must be between 8 to 128 character only");
+  };
+
+  if (Number.isNaN(keyLength)) {
+    alert('Password length must be provided as a number')
+    return null;
+  };
+
+  if (!upperCase && !lowerCase && !specialCharacters && !numerals)
+  { 
+    alert("You must choose at least one character type");
+    return null;
+  };
+
+  if (upperCase) {multiSelect.push(upperCaseCriteria)};
+  if (lowerCase) {multiSelect.push(lowerCaseCriteria)};
+  if (numerals) {multiSelect.push(numeralsCriteria)};
+  if (specialCharacters) {multiSelect += specialChacCriteria};
+
+  let finalpassword = ''
+  for (let i = 0; i < keyLength; i++) {
+    let random =[Math.floor(Math.random()*multiSelect.length)];
+    finalpassword = finalpassword + multiSelect[random];
+  };
+
+  return finalpassword;
+};
+
+// this is all correct do not disturb 
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+};
+
+generateBtn.addEventListener("click", writePassword);
+
+//new repo
